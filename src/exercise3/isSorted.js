97d429087097c5ce76b8bc7993969ef8e6739f1b
Baek2back <dumbuk12@naver.com>
2020-12-05 14:16:38 +0900

    fix: Cover empty array in isSorted

--- src/exercise3/isSorted.js
+++ src/exercise3/isSorted.js
@@ -2,7 +2,7 @@ function isSorted(array) {
   for (let i = 0; i < array.length - 1; i++) {
     if (array[i] > array[i + 1]) return false;
   }
-  return true;
+  return !!array.length;
 }
 
 console.log(isSorted([1, 2, 3, 4, 5]));
